2eef2ea1c38ca705dd57c3d4ab7482256daff3dc
memsec <zbc.kun@gmail.com>
2023-02-05 05:19:14 +0300

    Editor file Story and viewcontroller

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,9 +14,25 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    let story0 = "You see a fork in the road."
+    let choice1 = "Take a left."
+    let choice2 = "Take a right."
+    let example = [Story(title: "You see a fork in the road.", choice1: " tale a left.", choice2:"Take a right"),
+                         Story(title: "You see a tiger.", choice1: "Shout for help.", choice2: "Play dead"),
+                         Story(title: "You a tresaure chest.",
+                               choice1: "Open it.",
+                               choice2: "Check for tramps")]
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        storyLabel.text = example[0].title
+        choice1Button.setTitle(example[0].choice1, for: .normal)
+        choice2Button.setTitle(example[0].choice2, for: .normal)
+    }
+    
+    @IBAction func choiceMade(_ sender: UIButton){
+        
     }
 
 
--- Destini-iOS13/Model/Story.swift
+++ Destini-iOS13/Model/Story.swift
@@ -9,3 +9,8 @@
 import Foundation
 
 
+struct Story {
+    var title = String()
+    var choice1 = String()
+    var choice2 = String()
+}
