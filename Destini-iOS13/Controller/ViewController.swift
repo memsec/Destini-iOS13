130605b3691c06530852b43cd69ef7130393215c
memsec <zbc.kun@gmail.com>
2023-02-05 06:07:01 +0300

    add new code

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -1,10 +1,3 @@
-//
-//  ViewController.swift
-//  Destini-iOS13
-//
-//  Created by Angela Yu on 08/08/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
 
 import UIKit
 
@@ -14,27 +7,34 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
-    let story0 = "You see a fork in the road."
-    let choice1 = "Take a left."
-    let choice2 = "Take a right."
-    let example = [Story(title: "You see a fork in the road.", choice1: " tale a left.", choice2:"Take a right"),
-                         Story(title: "You see a tiger.", choice1: "Shout for help.", choice2: "Play dead"),
-                         Story(title: "You a tresaure chest.",
-                               choice1: "Open it.",
-                               choice2: "Check for tramps")]
-
+    var storybrain = StoryBrain()
+    
+//    let story0 = "You see a fork in the road."
+//    let choice1 = "Take a left."
+//    let choice2 = "Take a right."
+//    let example = [Story(title: "You see a fork in the road.", choice1: " tale a left.", choice2:"Take a right"),
+//                            Story(title: "You see a tiger.", choice1: "Shout for help.", choice2: "Play dead"),
+//                            Story(title: "You a tresaure chest.", choice1: "Open it.", choice2: "Check for tramps")]
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        storyLabel.text = example[0].title
-        choice1Button.setTitle(example[0].choice1, for: .normal)
-        choice2Button.setTitle(example[0].choice2, for: .normal)
+        updateUI()
+//        storyLabel.text = example[0].title
+//        choice1Button.setTitle(example[0].choice1, for: .normal)
+//        choice2Button.setTitle(example[0].choice2, for: .normal)
     }
     
     @IBAction func choiceMade(_ sender: UIButton){
+        storyNo = storybrain.nextStory(userChoice: sender.currentTitle!)
+        updateUI()
         
     }
 
+    func updateUI() {
+        storyLabel.text = storybrain.stories[storyNo].title
+        choice1Button.setTitle(storybrain.stories[storyNo].choice1, for: .normal)
+        choice2Button.setTitle(storybrain.stories[storyNo].choice2, for: .normal)
+    }
 
 }
 
--- Destini-iOS13/Model/Story.swift
+++ Destini-iOS13/Model/Story.swift
@@ -1,10 +1,4 @@
-//
-//  Sotry.swift
-//  Destini-iOS13
-//
-//  Created by Angela Yu on 08/08/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
+
 
 import Foundation
 
@@ -12,5 +6,7 @@ import Foundation
 struct Story {
     var title = String()
     var choice1 = String()
+    var choice1Destination: Int
     var choice2 = String()
+    var choice2Destination: Int
 }
